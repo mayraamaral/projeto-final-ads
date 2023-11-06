326d4a70c03fd364c8b3814cea1e8769f636b479
Mayra Amaral <mayracunha18@gmail.com>
2023-11-06 06:28:55 -0300

    feat: projeto finalizado

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Guia de Acessibilidade no Desenvolvimento Front-End
 
 Autora: Mayra Amaral de Miranda Cunha.  
-Proposta de Trabalho Final da disciplina de Atividades Extensionistas I do curso de Análise e Desenvolvimento de Sistemas da UNINTER.
+Trabalho Final da disciplina de Atividades Extensionistas do curso de Análise e Desenvolvimento de Sistemas da UNINTER.
 
 **Objetivo:** Este projeto visa reunir todas as principais formas de garantir a acessibilidade no desenvolvimento front-end dos sites, assegurando que todas as pessoas consigam interagir com os sites que seguirem essas práticas.
 
@@ -11,5 +11,5 @@ O único propósito deste projeto é guiar desenvolvedores front-end no desenvol
 ## Tecnologias utilizadas
 
 - Desenvolvimento: React.js;
-- Estilização: Styled Components;
+- Estilização: CSS;
 - Deploy: Vercel.
--- src/App.js
+++ src/App.js
@@ -1,23 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1>Por Todos - Um Guia de Desenvolvimento Inclusivo</h1>
+      <span>
+        Práticas para tornar sites acessíveis <strong>por todos</strong>.
+      </span>
+      <span className="dica">
+        <strong>Dica:</strong> Para acessar, clique nos títulos e verifique os
+        textos de apoio.
+      </span>
+      <h2>Recomendações</h2>
+      <details>
+        <summary>Imagens</summary>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Todas as imagens devem ter um atributo <strong>alt</strong>, pois
+          serão alternativas não-visuais no formato de texto, para o caso de a
+          imagem não estar disponível ou para que essas alternativas sejam
+          utilizadas por leitores de tela.
         </p>
+      </details>
+      <details>
+        <summary>Áudio e vídeo</summary>
+        <p>
+          Fornecer alternativas para conteúdos baseados em tempo (como vídeo e
+          áudio), seja a transcrição, resumo ou explicação do que se trata.
+        </p>
+      </details>
+      <details>
+        <summary>Responsividade</summary>
+        <p>
+          Garantir que a aplicação se comportará de forma correta em quaisquer
+          tamanhos de tela, sejam elas: monitores, notebooks, tablets,
+          celulares, etc.
+        </p>
+      </details>
+      <details>
+        <summary>Interface</summary>
+        <p>
+          O uso de cores deve favorecer a distinção entre o texto e o plano de
+          fundo, de forma que seja possível ler claramente. Observe o contraste
+          mínimo para que seja possível utilizar o site ou aplicação.
+        </p>
+      </details>
+      <details>
+        <summary>Acessibilidade a partir do teclado</summary>
+        <p>
+          É essencial garantir que a aplicação é navegável a partir do teclado,
+          para isso, você pode realizar testes de usabilidade.
+        </p>
+      </details>
+      <details>
+        <summary>
+          Não criar conteúdo de uma forma conhecida por causar convulsões
+        </summary>
+        <p>
+          Não incluir nenhum conteúdo que pisque mais de três vezes em um
+          segundo.
+        </p>
+      </details>
+      <details>
+        <summary>Ajudar o usuário a navegar</summary>
+        <p>
+          Garantir que o usuário conseguirá navegar e localizar conteúdos
+          facilmente.
+        </p>
+      </details>
+      <details>
+        <summary>Conteúdo legível</summary>
+        <p>
+          Tornar o conteúdo textual legível e compreensível. Algumas boas
+          práticas são:
+        </p>
+        <ul>
+          <li>Definir o idioma da aplicação</li>
+          <li>
+            Utilizar linguagem clara e simples ou fornecer um mecanismo para
+            identificar palavras ou expressões incomuns.
+          </li>
+          <li>Fornecer o significado de abreviações.</li>
+        </ul>
+      </details>
+      <details>
+        <summary>Funcionamento previsível</summary>
+        <p>
+          Fazer com que o funcionmento e comportamento da aplicação seja quase
+          previsível, de forma a facilitar a navegação do usuário.
+        </p>
+      </details>
+      <details>
+        <summary>Erros: Evitar e Corrigir</summary>
+        <p>
+          Ajudar os usuários a evitar erros e, em caso de ocorrência,
+          corrigí-los. Isso pode ser alcançado a partir de testes de software,
+          para garantir a qualidade da aplicação.
+        </p>
+      </details>
+      <details>
+        <summary>Compatibilidade e semântica</summary>
+        <p>
+          Ao utilizar linguagens de marcação, garantir que estão sendo
+          utilizados elementos semânticos, de início e fim, e aninhados de
+          acordo com as respectivas especificações.
+        </p>
+      </details>
+      <h2>Referências</h2>
+      <p>
+        Todo o conteúdo disponível neste site foi construído com base no Web
+        Content Accessibility Guidelines (WCAG) 2.0, disponibilizado{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://www.w3.org/Translations/WCAG20-pt-br"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          Learn React
+          neste link
         </a>
-      </header>
+        .
+      </p>
     </div>
   );
 }
